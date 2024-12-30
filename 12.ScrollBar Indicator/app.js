f53471009d8a2483fa75aac62650b5078c8f0ba4
Sanjaykumar <sanjaykumar679593@gmail.com>
2024-12-30 14:55:18 +0530

    ScrollBar Indicator Component Added

---
+++ 12.ScrollBar Indicator/app.js
@@ -0,0 +1,15 @@
+let indicator = document.querySelector(".scroll-indicator .progress");
+let scrollHeight =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+// console.log(scrollHeight);
+
+window.addEventListener("scroll", scroll);
+
+function scroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrolled = (scrollTop / scrollHeight) * 100;
+  if (indicator) {
+    indicator.style.width = `${scrolled}%`;
+  }
+}
